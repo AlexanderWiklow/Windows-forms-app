ed68b7cda7e992480654378541e05284b8146dea
Alexander <a.wiklow@gmail.com>
2022-01-12 20:31:39 +0100

    Create a windows forms app

---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Messages", typeof(String));
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Messages"].Visible = false;
+
+            dataGridView1.Columns["Title"].Width = 140;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(textBox1.Text, textBox2.Text);
+
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            if (index > -1)
+
+                textBox1.Text = table.Rows[index].ItemArray[0].ToString();
+
+            textBox2.Text = table.Rows[index].ItemArray[1].ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
